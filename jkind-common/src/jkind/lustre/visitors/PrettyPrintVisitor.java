41eccc2e233f612a122a6b33a83efb1fa5ecf466
John Backes <john.backes@rockwellcollins.com>
2015-10-29 15:48:03 -0500

    fixing something with contracts

--- jkind-common/src/jkind/lustre/visitors/PrettyPrintVisitor.java
+++ jkind-common/src/jkind/lustre/visitors/PrettyPrintVisitor.java
@@ -466,7 +466,7 @@ public class PrettyPrintVisitor implements AstVisitor<Void, Void> {
 
 	@Override
 	public Void visit(Contract contract) {
-		write("--@contract : "+contract.name+";");
+		//write("--@contract : "+contract.name+";");
 		newline();
 		for(Expr expr : contract.requires){
 			write("--@require ");
