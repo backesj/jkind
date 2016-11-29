a49fcdb1a438fbb07cc270e205b2bbe6612a1e55
John Backes <john.backes@rockwellcollins.com>
2016-11-29 14:08:28 -0600

    stashing

--- jkind-api/src/jkind/api/JKindApi.java
+++ jkind-api/src/jkind/api/JKindApi.java
@@ -211,6 +211,7 @@ public class JKindApi extends KindApi {
 
 		args.add(lustreFile.toString());
 
+		System.out.println(args);
 		ProcessBuilder builder = new ProcessBuilder(args);
 		ApiUtil.addEnvironment(builder, environment);
 		builder.redirectErrorStream(true);
